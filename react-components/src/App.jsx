76958a260b9e3ecae2a043a3639b257c0f24205e
Gargenziano03 <gargenziano03@gmail.com>
2024-11-12 16:14:34 +0100

    add import AppMain

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -1,4 +1,5 @@
 import AppHeader from './Components/Appheader'
+import AppMain from './Components/AppMain'
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <AppHeader />
+      <AppMain />
     </>
   )
 }
