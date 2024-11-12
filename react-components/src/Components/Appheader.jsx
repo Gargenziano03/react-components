c4b1867ca7fa89ba5932554aec50f121c71d0da5
Gargenziano03 <gargenziano03@gmail.com>
2024-11-12 16:11:25 +0100

    add title header + css

--- react-components/src/Components/Appheader.jsx
+++ react-components/src/Components/Appheader.jsx
@@ -1,4 +1,10 @@
 export default function AppHeader() {
     //logic
+    const title = 'il mio blog'
     //markup
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    )
 }
